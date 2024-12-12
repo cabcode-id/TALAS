c7e7845a43adca9ea01feb136468f3265d4cf1ae
Gustia Rahmi <gustiarahmy14@gmail.com>
2024-12-12 22:00:42 +0700

    Update same files

--- CloudComputing/frontend-talas/src/App.jsx
+++ CloudComputing/frontend-talas/src/App.jsx
@@ -6,9 +6,12 @@ import Navbar from './components/Navbar';
 import FormLogin from './components/FormLogin';
 import News from './components/News';
 import ToggleMenu from './components/ToggleMenu';
-// import About from './pages/About';
 import About from './pages/about';
-
+import Finance from "./pages/finance";
+// import Footer from "./components/Footer"
+import International from "./pages/International";
+import Science from './pages/Science';
+import Discover from './pages/DiscoverMoreTopic';
 export default function App() {
   return (
     <Router>
@@ -16,6 +19,8 @@ export default function App() {
         {/* Navbar dan DarkModeToggle selalu muncul di semua halaman */}
         <Navbar />
         <DarkModeToggle />
+        
+        {/* <Finance/> */}
 
         {/* Routes menentukan halaman berdasarkan URL */}
         <Routes>
@@ -31,7 +36,15 @@ export default function App() {
           <Route path="/bias/*" element={<PageSwitch />} />
           <Route path="/login" element={<FormLogin />} />
           <Route path="/about" element={<About />} />
+          <Route path="/finance" element={<Finance />} />
+          <Route path="/international" element={<International />} />
+          <Route path="/science" element={<Science />} />
+          <Route path="/discover" element={<Discover/>} />
+          {/* <Route path="/finance" element={<Finance />} />
+          <Route path="/finance" element={<Finance />} /> */}
         </Routes>
+
+        {/* <Footer /> */}
       </div>
     </Router>
   );
