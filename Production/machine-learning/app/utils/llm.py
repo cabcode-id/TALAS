d44195a02efb1b37fcbda940633082d5d030a9b5
Iga Narendra <iga.narendra@gmail.com>
2025-04-21 11:55:42 +0800

    fix liberal vs conservative processing

--- Production/machine-learning/app/utils/llm.py
+++ Production/machine-learning/app/utils/llm.py
@@ -23,13 +23,14 @@ def create_documents(df):
         metadata = {'title': row['title']}
 
         if 'bias' in row and not pd.isnull(row['bias']):
-            metadata['bias'] = 'biased' if row['bias'] == 1 else 'not biased'
+            metadata['bias'] = row['bias']
 
         if 'hoax' in row and not pd.isnull(row['hoax']):
             metadata['hoax'] = row['hoax']
 
         if 'ideology' in row and not pd.isnull(row['ideology']):
-            metadata['ideology'] = 'liberalism' if row['ideology'] == 1 else 'conservative'
+            # metadata['ideology'] = 'liberalism' if row['ideology'] == 1 else 'conservative'
+            metadata['ideology'] = row['ideology']
 
         document = Document(
             text=row['content'],
@@ -47,11 +48,16 @@ def create_summary(documents):
     
     summarizeQuery = """
     Create a short, detailed, and factual summary of the articles.
-    information regarding: 
-    it's bias (0: not biased/neutral, 1: biased), 
-    hoax (0: is factual, 1: has hoax), 
-    and whether it's ideology (liberal or conservative)
+    Include important informations from the articles. 
+    Use the framework of a good paragraph that answers the what, when, who, where, which, and how inside the summary.
+    
+    metadata related to each article: 
+    likelihood to be read from a narrow point of view (0: not biased/neutral, 1: biased), 
+    likelihood of this article to be re-written into missleading hoaxes (0: is factual, 1: has more likely to be a hoax), 
+    and whether it's writing style ideology (a bit provocative like a liberal (the closer the value to 1) or trying to maintain what is already exist like a conservative (the closer the value to 0))
+
     which are all detected using machine learning models are also included.
+    
     Do NOT discuss about the article's hoax, bias, or political view. 
     Do NOT rely on previous knowledge.
     Use Indonesian language. 
@@ -107,8 +113,10 @@ def create_analysis(query_engine, cuan_result):
     return response.response + cuanResponse  # Avoid accessing response2 if it's not set
 
 def summarize_article(documents):
+    ### different summary from each ideology
     document_liberalism = [doc for doc in documents if doc.metadata['ideology'] == 'liberalism']
     document_conservative = [doc for doc in documents if doc.metadata['ideology'] == 'conservative']
+
 
     summary_liberalism = create_summary(document_liberalism)
     summary_conservative = create_summary(document_conservative)
