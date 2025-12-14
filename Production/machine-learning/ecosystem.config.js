2bbea3b3073d24b3c8575c401fffe281926d7e3d
Iga Narendra <iga.narendra@gmail.com>
2025-12-14 14:01:51 +0700

    fix cors issue after migration

--- Production/machine-learning/ecosystem.config.js
+++ Production/machine-learning/ecosystem.config.js
@@ -2,15 +2,18 @@ module.exports = {
   apps: [
     {
       name: "talas-ml",
-      script: "run.py",
-      interpreter: "/home/ubuntu/venv/bin/python3",
+      script: "/home/ubuntu/TALAS/Production/machine-learning/venv/bin/python3",
+      args: "run.py",
+      interpreter: "none",
       cwd: "/home/ubuntu/TALAS/Production/machine-learning",
       watch: false,
       autorestart: true,
       max_restarts: 5,
       env: {
-        FLASK_ENV: "production"
-      }
+        FLASK_ENV: "production",
+      },
+      out_file: "/home/ubuntu/.pm2/logs/talas-ml-out.log",
+      error_file: "/home/ubuntu/.pm2/logs/talas-ml-error.log",
     }
   ]
-}
+};
--- Production/machine-learning/run.py
+++ Production/machine-learning/run.py
@@ -1,6 +1,11 @@
 #!/home/ubuntu/venv/bin/python3
-
+from flask_cors import CORS
 from app import app
+
+from flask_cors import CORS
+
+CORS(app, origins=["https://migration.talas.news", "http://migration.talas.news"])
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
